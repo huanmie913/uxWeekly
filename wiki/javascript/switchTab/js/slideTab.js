93a0b9e2c796cb00a129bed1e8e9502669b105ab
f2er <uezheng@gmail.com>
2012-03-23 14:37:53 +0800

    update slide

--- wiki/javascript/switchTab/js/slideTab.js
+++ wiki/javascript/switchTab/js/slideTab.js
@@ -9,14 +9,15 @@ function TabSlide(ctg){
 				_eventType : ctg.eventType,
 				_currentClass : ctg.currentClass,
 				_nornalClass : ctg.nornalClass,
-				_n		   : ctg.n
+				_n		   : ctg.n,
+				_opacityStart : ctg.opacityStart,
+				_opacityEnd : ctg.opacityEnd,
+				_step : ctg.step
 			}
 			if( !( this instanceof TabSlide )){
 				return new TabSlide(ctg);
 			}
 			this._timer = null;
-			this._opacityStart = 10;
-			this._opacityEnd = 100;
 			this.init();
 		}
 		TabSlide.prototype = {
@@ -46,36 +47,42 @@ function TabSlide(ctg){
 					return computedStyle;
 				}
 			},
+			clearTimer : function(){
+				var that = this;
+				if( that._timer ){
+					clearInterval( that._timer );
+				}
+				that._timer = null;
+			},
 			animateShow : function(obj){
 				var that = this;
-				function animate(i){
-					that._timer = setInterval(function(){
-							obj.style.opacity = i/100;
-							obj.style.filter = "alpha(opacity="+i+")";
-						},50)
-				}
-				//clearInterval( that._timer );
-				if( that._opacityStart > that._opacityEnd ){
-					for( var i=that._opacityEnd; i<= that._opacityStart;i--){
-						animate(i);
+					time = that.option._opacityStart;
+				function show(){
+					time += that.option._step;
+					obj.style.opacity = time/100;
+					obj.style.filter = "alpha(opacity="+time+")";
+					if( time >= that.option._opacityEnd ){
+						that.clearTimer();
+						time = that.option._opacityStart ;
 					}
-				}else if( that._opacityStart < that._opacityEnd ){
-					for( var i=that._opacityStart; i<=that._opacityEnd;i++){
-						animate(i);
-					}	
 				}
+				that.clearTimer();
+				that._timer = setInterval(show,50)
 			},
-			doTarget : function(  ){
+			doTarget : function(){
 				var that = this,
 					_obj = document.getElementById(that.option._id),
 					_objArray = _obj.getElementsByTagName("div");
-					_objArray[ that.option._n ].className = " "+that.option._currentClass;
 					
-				var _content = _objArray[ that.option._n ].getElementsByTagName('dd')[0];
-				var _width = parseInt(that.getCss(_content,'width'));
+					for( var i = 0,len =_objArray.length;i<len;i++ ){
+						
+					}
+					_objArray[ that.option._n ].className = " "+that.option._currentClass;
+					_objArray[ that.option._n ].getElementsByTagName('dd')[0].style.opacity = 1;
+					_objArray[ that.option._n ].getElementsByTagName('dd')[0].style.filter = "alpha(opacity=100)";
 				
 				that.addEvent( _obj, that.option._eventType,function(event){
-					clearInterval( that._timer );
+					
 					var _ev = event || window.event,
 						_target = _ev.target || _ev.srcElement;
 					if( _target.nodeName.toLowerCase() != "dt"){
@@ -83,9 +90,14 @@ function TabSlide(ctg){
 					}
 					var _index = that.index( _target );
 					for( var j=0,_len = _objArray.length;j<_len;j++){
+						var _dd = _objArray[j].getElementsByTagName('dd')[0];
+						_dd.style.opacity = that.option._opacityStart/100;
+						_dd.style.filter = "alpha(opacity="+that.option._opacityStart+")";
 						_objArray[j].className = ( j != _index ) ? that.option._nornalClass : that.option._nornalClass+" "+that.option._currentClass;
+						var _targetImg = _objArray[_index].getElementsByTagName('dd')[0];
+						that.animateShow(_targetImg)
 					}
-					//that.animateShow(_objArray[_index]);
+					
 				})
 			},
 			init: function(){
