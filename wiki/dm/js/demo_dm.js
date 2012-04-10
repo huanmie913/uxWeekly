52562e80fba816df34972c73dc4676851e427e9e
f2er <uezheng@gmail.com>
2012-04-10 17:22:09 +0800

    update dm

---
+++ wiki/dm/js/demo_dm.js
@@ -0,0 +1,30 @@
+﻿/*
+ * To: wiki/dm/
+ * Time:2012-04-10
+ * authro :f2er 
+* */
+
+
+var dm_f2er = dm_f2er || {};
+
+dm_f2er = {
+    triggerResource : function(){
+       var _dm = document.getElementById("dm"),
+           _btnLookResource = _dm.querySelectorAll("span.look_reource"),
+           _resourceBox = _dm.querySelectorAll("div.demo_resource");
+        for( var i = 0,len= _btnLookResource.length;i<len;i++){
+            _btnLookResource[i].onclick = (function(n){
+                var flag = 0;
+                return function(){
+                    if( flag == 0 ){
+                        _resourceBox[n].style.display = "block";
+                        flag = 1;
+                    }else{
+                        _resourceBox[n].style.display = "none";
+                        flag = 0;
+                    }
+                }
+            })(i);
+        }
+    }
+};
