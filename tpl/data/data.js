5b7f1bf3b3d53bcfa9052668792c190a3ecf2a04
f2er <uezheng@gmail.com>
2012-05-14 14:13:22 +0800

    add index

---
+++ tpl/data/data.js
@@ -0,0 +1,12 @@
+﻿/*
+ * @4399 UED
+ * @update by : 情封
+ * @time : 2012-05-14
+*/
+
+var ued = {
+	article : [
+		{ title:"Android4.0设计规范",href:"article/201205/android4.0.html" },
+		{ title:"Google HTML/CSS代码风格指南",href:"article/201205/googleHtmlCss.html"}
+	]
+}
