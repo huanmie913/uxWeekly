1f6fb16917eb8bf7a3e357a65a32c9f1d33abeca
f2er <uezheng@gmail.com>
2012-06-14 11:04:40 +0800

    update carousel

---
+++ wiki/javascript/Page/js/Pager.js
@@ -0,0 +1,309 @@
+
+/*
+ * 更新数据
+*/
+var DHlist = {
+		0 : [
+				{ num : 25,href : "http://www.baidu.com", title : "0李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "1" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "2" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "3" }
+			],
+		1  : [
+				{ num : 25,href : "http://www.baidu.com", title : "1李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "1" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "2" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "3" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "4" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "5" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "6" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "7" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "8" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"}
+				/*{ num : 30,href : "http://www.google.com", title : "9" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "10" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍22"}*/
+			],
+		2  : [
+				{ num : 25,href : "http://www.baidu.com", title : "2李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "1" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "2" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "3" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "4" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "5" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "6" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"}
+			],
+		3  : [
+				{ num : 25,href : "http://www.baidu.com", title : "3李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "1" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "2" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "3" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "4" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "5" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "6" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "7" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "8" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "9" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"}
+			],
+		4 : [
+				{ num : 25,href : "http://www.baidu.com", title : "4李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "1" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "2" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "3" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "4" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "5" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "6" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "7" }
+			],
+		5  : [
+				{ num : 25,href : "http://www.baidu.com", title : "5李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "1" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "2" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "3" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "4" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "5" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "6" }
+			],
+		6  : [
+				{ num : 25,href : "http://www.baidu.com", title : "6李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "1" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "2" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "3" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "4" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "5" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "6" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "7" },
+				{ num : 25,href : "http://www.baidu.com", title : "6李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "1" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "2" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "3" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "4" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "5" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "6" },
+				{ num : 25,href : "http://www.baidu.com", title : "李洛克的青春忍"},
+				{ num : 30,href : "http://www.google.com", title : "7" }
+			]
+		
+	};
+	
+/*
+ * Page : 分页
+*/
+(function(win,undefined){
+	var doc = win.document;
+	var QF = QF || {};
+	QF = {
+		extend : function(destination,source){
+			for( var prop in source ){
+				if( source.hasOwnProperty(prop) ){
+					destination[prop] = source[prop];
+				}
+			}
+			return destination;
+		},
+		$ : function(id){
+			return typeof id == "string" ? doc.getElementById(id) : id;
+		},
+		IsFunction : function(callback){
+			if( {}.toString.call(callback) == "[object Function]"){
+				return true;
+			}else{
+				return false;
+			}
+		},
+		addEvent : function(otarget,otype,ofn){
+			if(otarget.addEventListener){
+				otarget.addEventListener(otype,ofn,false);
+			}else if(otarget.attachEvent){
+				otarget.attachEvent('on'+otype,function(){
+					ofn.call(otarget);
+				});
+			}else{
+				otarget["on"+otype] = ofn;
+			}
+		},
+		getEvent : function(e){
+			return e || window.event;
+		},
+		getTarget : function(e){
+			return e.target || e.srcElement;
+		},
+		getIndex : function(target,obj){
+			for( var j=0,_len=obj.length;j<_len;j++){
+				if(obj[j]==target){
+					return j;	
+				}
+			}
+		}
+	}
+	/*WeekTab*/
+	function WeekTab(ctg){
+		this.opt = {
+			tid       : "j-tabweek",
+			bid       : "j-qlist",
+			tag       : "li",
+			curClass  : "cur",
+			eventType : "click",
+			tnum      : 3,
+			dataArr   : DHlist,
+			callback  : function(){}
+		}
+		this.option = QF.extend(this.opt,ctg || {});
+		if( !(this instanceof arguments.callee) ){
+			return new arguments.callee(ctg);
+		}
+		this.init();
+	}
+	WeekTab.prototype = {
+		constructor : WeekTab,
+		getData : function(n){
+			var that = this,
+				_aList = that.option.dataArr[n],
+				_html = "";
+			for( var i=0,len = _aList.length;i<len;i++ ){
+				var _list = _aList[i];
+				_html += '<li><span class="nr">第<em>'+_list.num+'</em>集</span><a href="'+_list.href+'">'+(i< that.option.tnum ? '<span class="tt">&middot;</span>' : '&middot;')+_list.title+'</a></li>';
+			}	
+			QF.$( that.option.bid ).innerHTML = _html;
+		},
+		trigger : function(n){
+			var that = this,
+				_aTag = QF.$( that.option.tid ).getElementsByTagName( that.option.tag );
+			for( var len = _aTag.length-1;len>=0;len--){
+				_aTag[len].className = (len == n ) ? that.option.curClass : "";
+			}
+			that.getData(n);
+			QF.IsFunction(that.option.callback) && (that.option.callback)();
+		},
+		init : function(){
+			var that = this,
+				_odate = new Date().getDay(),
+				_aTag = QF.$( that.option.tid ).getElementsByTagName( that.option.tag );
+				that.trigger(_odate);
+
+			QF.addEvent( QF.$( that.option.tid ),that.option.eventType,function(e){
+				var _ev = QF.getEvent(e),
+					_target = QF.getTarget(_ev);
+				if(_target.nodeName.toLowerCase()!="li"){
+					return;
+				}
+				_odate = QF.getIndex(_target,_aTag);
+				that.trigger(_odate);
+			})
+		}
+	}
+	
+	/*Page*/
+	function Page(ctg){
+		this.opt = {
+			maxNum     : 8,
+			bid        : 'j-qlist',
+			pid        : 'j_qpage',
+			curClass   : 'cur',
+			Initnum    : 1, // 初始化
+			eventType  : "click"
+		}
+		this.option = QF.extend(this.opt,ctg||{});
+		if( !(this instanceof arguments.callee) ){
+			return new arguments.callee(ctg);
+		}
+		this.init();
+	}
+	Page.prototype = {
+		constructor : Page,
+		initPage : function(n,m){
+			var that = this,
+				_html = "";
+			for(var i =1;i<=n;i++){
+				_html +='<li class='+(m==i?"cur":"")+'>'+i+'</li>';
+			}
+			QF.$(that.option.pid).innerHTML = _html;
+		},
+		goPage : function(n){
+			var that = this,
+				len = QF.$( that.option.bid ).children.length;
+			for( var i=0;i<len;i++){
+				QF.$( that.option.bid ).children[i].style.display = ( i>=that.option.maxNum*(n-1) && i<that.option.maxNum*n ) ? "block" : "none";
+			}
+			
+		},
+		init : function(){
+			var that = this,
+				_children = QF.$( that.option.bid ).children,
+				_len = _children.length,
+				_pnum = 0;//总页数
+			if(_len < that.opt.maxNum){
+				_pnum = 1;
+			}
+			if( _len%that.opt.maxNum == 0 ){
+				_pnum = _len/that.opt.maxNum;
+			}else{
+				_pnum = Math.floor(_len/that.opt.maxNum)+1;
+			}
+			that.initPage(_pnum,that.option.Initnum);
+			that.goPage(that.option.Initnum);
+			var _pchildren = QF.$( that.option.pid ).children;
+			QF.addEvent( QF.$( that.option.pid ),that.option.eventType,function(e){
+				var _ev = QF.getEvent(e),
+					_target = QF.getTarget(_ev);
+				if(_target.nodeName.toLowerCase()!="li"){
+					return;
+				}
+				var _index = QF.getIndex(_target,_pchildren);
+				that.goPage(_index+1);
+				for( var j=0;j<_pchildren.length;j++){
+					QF.$( that.option.pid ).children[j].className = (j==_index) ? that.option.curClass : "";
+				}
+			})
+		}
+	}
+	win.WeekTab = WeekTab;
+	win.Page = Page;
+})(window);
