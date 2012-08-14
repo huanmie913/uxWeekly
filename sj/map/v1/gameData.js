d4cb1b059e39e6cb4bda52061519623ff6c6cc66
f2er <uezheng@gmail.com>
2012-08-14 14:05:51 +0800

    update map

---
+++ sj/map/v1/gameData.js
@@ -0,0 +1,203 @@
+
+/*
+ * gameIndex 
+ * 2012-06-28
+*/
+var gameIndex = [ 
+                    {timestamp :1280977330748} ,
+                    {initnum : 267342912 },
+                    {info :[
+                			{
+                				"area":["xj","新疆",3],
+                				"game":["造梦西游|150","http://s4.img4399.com/1/apps/800111.gif"],
+                				"time" : ["周天|150"],
+                                "position" : {x:310,y:260}
+                			},
+                			{
+                				"area":["xz","西藏",3],
+                				"game":["海贼王|10","http://s4.img4399.com/1/apps/800110.gif"],
+                				"time" : ["星期一|50"],
+                                "position" : {x:350,y:490}
+                			},
+                            {
+                                "area":["qh","青海",3],
+                                "game":["4399开心农场|50","http://s4.img4399.com/1/apps/800112.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:450,y:410}
+                            },
+                            {
+                                "area":["gs","甘肃",3],
+                                "game":["穿越三国|50","http://s4.img4399.com/1/apps/800105.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:520,y:410}
+                            },
+                            {
+                                "area":["nmg","内蒙古",3],
+                                "game":["疯狂宠物|50","http://s4.img4399.com/1/apps/800106.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:610,y:320}
+                            },
+                            {
+                                "area":["hlj","黑龙江",5],
+                                "game":["美食大战老鼠|50","http://s4.img4399.com/1/apps/800108.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:800,y:200}
+                            },
+                            {
+                                "area":["jl","吉林",5],
+                                "game":["神魔斗|50","http://s4.img4399.com/1/apps/100225.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:795,y:240}
+                            },
+                            {
+                                "area":["ln","辽宁",5],
+                                "game":["4399神仙道|50","http://s4.img4399.com/1/apps/800103.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:780,y:285}
+                            },
+                            {
+                                "area":["sc","四川",6],
+                                "game":["4399火影世界|50","http://s4.img4399.com/1/apps/800111.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:520,y:500}
+                            },
+                            {
+                                "area":["sx","陕西",5],
+                                "game":["洛克王国|50","http://s4.img4399.com/1/apps/100222.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:590,y:350}
+                            },
+                            {
+                                "area":["js","江苏",6],
+                                "game":["海贼王|10","http://s4.img4399.com/1/apps/1005122.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:750,y:460}
+                            },
+                            {
+                                "area":["zj","浙江",7],
+                                "game":["飘渺西游|50","http://s4.img4399.com/1/apps/100150.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:760,y:500}
+                            },
+                            {
+                                "area":["tw","台湾",5],
+                                "game":["花园抢战|50","http://s4.img4399.com/1/apps/100162.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:800,y:580}
+                            },
+                            {
+                                "area":["ah","安徽",4],
+                                "game":["百炼成仙|50","http://s4.img4399.com/1/apps/100227.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:720,y:480}
+                            },
+                            {
+                                "area":["fj","福建",8],
+                                "game":["海贼大冒险|50","http://s4.img4399.com/1/apps/100192.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:755,y:570}
+                            },
+                            {
+                                "area":["jx","江西",5],
+                                "game":["飞天忍者猫|50","http://s4.img4399.com/1/apps/100194.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:700,y:530}
+                            },
+                            {
+                                "area":["gd","广东",9],
+                                "game":["穿越三国|50","http://s4.img4399.com/1/apps/100212.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:670,y:630}
+                            },
+                            {
+                                "area":["hn","海南",6],
+                                "game":["找茬小妹|50","http://s4.img4399.com/1/apps/100132.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:620,y:690}
+                            },
+                            {
+                                "area":["gx","广西",4],
+                                "game":["喂鲸鱼|50","http://s4.img4399.com/1/apps/100170.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:590,y:630}
+                            },
+                            {
+                                "area":["hun","湖南",6],
+                                "game":["丘比特之箭|50","http://s4.img4399.com/1/apps/155.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:650,y:540}
+                            },
+                            {
+                                "area":["gz","贵州",5],
+                                "game":["移行换位|50","http://s4.img4399.com/1/apps/800100.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:560,y:575}
+                            },
+                            {
+                                "area":["yn","云南",5],
+                                "game":["兔子跳铃铛|50","http://s4.img4399.com/1/apps/123.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:500,y:600}
+                            },
+                            {
+                                "area":["nx","宁夏",3],
+                                "game":["大厨连连看|50","http://s4.img4399.com/1/apps/149.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:550,y:360}
+                            },
+                            {
+                                "area":["shx","山西",5],
+                                "game":["障碍马拉松|50","http://s4.img4399.com/1/apps/163.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:635,y:380}
+                            },
+                            {
+                                "area":["hen","河南",5],
+                                "game":["黄金矿工|50","http://s4.img4399.com/1/apps/100181.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:665,y:430}
+                            },
+                            {
+                                "area":["hb","湖北",6],
+                                "game":["宠物连连看|50","http://s4.img4399.com/1/apps/174.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:670,y:500}
+                            },
+                            {
+                                "area":["cq","重庆",8],
+                                "game":["跑跑卡丁车|50","http://s4.img4399.com/1/apps/151.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:565,y:520}
+                            },
+                            {
+                                "area":["heb","河北",7],
+                                "game":["障碍马拉松|50","http://s4.img4399.com/1/apps/800110.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:670,y:365}
+                            },
+                            {
+                                "area":["tj","天津",8],
+                                "game":["史努比砍砖|50","http://s4.img4399.com/1/apps/800108.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:700,y:330}
+                            },
+                            {
+                                "area":["bj","北京",10],
+                                "game":["史努比滑雪|50","http://s4.img4399.com/1/apps/132.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:690,y:320}
+                            },
+                            {
+                                "area":["sd","山东",7],
+                                "game":["花样童趣跳绳|50","http://s4.img4399.com/1/apps/149.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:700,y:390}
+                            },
+                            {
+                                "area":["sh","上海",10],
+                                "game":["疯狂摩托车|50","http://s4.img4399.com/1/apps/141.gif"],
+                                "time" : ["星期一|50"],
+                                "position" : {x:780,y:470}
+                            }
+                    ]}
+    	       ];
