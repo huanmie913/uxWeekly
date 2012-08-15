01e2e17d12c825f3b792fd0b58e3db6d4c9e8774
f2er <uezheng@gmail.com>
2012-08-15 23:06:19 +0800

    update article

--- article/static/js/articleData.js
+++ article/static/js/articleData.js
@@ -5,16 +5,16 @@
 */
 var articleDataArray = [
 	{
-		"id":801,
+		"id":803,
 		"content" : {
-		  "from" : "http://www.ux265.net",
-		  "url" : "http://hi.baidu.com/lijing00333/item/1c28309d8b46c7d41e427118",
-		  "time" : "2012-08-09",
-		  "author" : "拔赤",
-		  "title" : "前端开发--十日谈",
+		  "from" : "http://coolshell.cn/articles/8031.html",
+		  "url" : "http://coolshell.cn/articles/8031.html",
+		  "time" : "2012-08-15",
+		  "author" : "陈皓",
+		  "title" : "InfoQ的ArchSummit大会对我的采访",
 		  "ajaxcontent" : {
 				"ajaxFlag" : true,
-				"ajaxSource" : "/article/201208/fexperience.html"
+				"ajaxSource" : "/article/201208/ArchSummit.html"
 		  }
 		}
 	},
@@ -29,6 +29,20 @@ var articleDataArray = [
 		  "ajaxcontent" : {
 				"ajaxFlag" : true,
 				"ajaxSource" : "/article/201208/dayWorkandTech.html"
+		  }
+		}
+	},
+	{
+		"id":801,
+		"content" : {
+		  "from" : "http://www.ux265.net",
+		  "url" : "http://hi.baidu.com/lijing00333/item/1c28309d8b46c7d41e427118",
+		  "time" : "2012-08-09",
+		  "author" : "拔赤",
+		  "title" : "前端开发--十日谈",
+		  "ajaxcontent" : {
+				"ajaxFlag" : true,
+				"ajaxSource" : "/article/201208/fexperience.html"
 		  }
 		}
 	}
