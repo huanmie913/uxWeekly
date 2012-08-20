7269951eec4ac9ae3e00ef3996de14644b29fdc8
f2er <uezheng@gmail.com>
2012-08-20 22:29:02 +0800

    update time

---
+++ time/static/js/timeData.js
@@ -0,0 +1,40 @@
+﻿/*
+ * to : 时间统计
+ * author : 情封
+ * time   : 2012-08-20 
+*/
+
+var timeData = [
+	{
+   		name : '蔡玉芳',
+   		value:[6,3,4,6,2,7],
+   		color:'#1f7e92',
+   		linewidth:1
+   	},
+	{
+   		name : '李俊',
+   		value:[5,3,5,7,9,5],
+   		color:'#f00',
+   		linewidth:1
+   	},
+	{
+   		name : '郭伟程',
+   		value:[5,4,5,7,2,7],
+   		color:'#1c9f1c',
+   		linewidth:1
+   	},
+	{
+   		name : '曹雪莲',
+   		value:[8,4,7,9,0,6],
+   		color:'#251c9f',
+   		linewidth:1
+   	},
+	{
+   		name : '郑国宝',
+   		value:[5,3,4,6,8,9],
+   		color:'#05ad74',
+   		linewidth:1
+   	}
+  ];
+
+var data_labels = ["星期一","星期二","星期三","星期四","星期五","星期六","星期日"];
