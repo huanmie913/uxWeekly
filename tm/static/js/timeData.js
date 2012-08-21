9b3c355f0e5760d5f4e73cfedab91defbafbeb0b
f2er <uezheng@gmail.com>
2012-08-21 21:26:44 +0800

    update tm

--- tm/static/js/timeData.js
+++ tm/static/js/timeData.js
@@ -6,32 +6,32 @@
 
 var timeData = [
 	{
-   		name : '蔡玉芳',
-   		value:[6,3,4,6,2,7],
+   		name : '小芳',
+   		value:[6,3],
    		color:'#1f7e92',
    		linewidth:1
    	},
 	{
-   		name : '李俊',
-   		value:[5,3,5,7,9,5],
+   		name : '俊GG',
+   		value:[5,7],
    		color:'#f00',
    		linewidth:1
    	},
 	{
-   		name : '郭伟程',
-   		value:[5,4,5,7,2,7],
+   		name : '小郭',
+   		value:[5,5],
    		color:'#1c9f1c',
    		linewidth:1
    	},
 	{
-   		name : '曹雪莲',
-   		value:[8,4,7,9,0,6],
+   		name : '雪莲',
+   		value:[8,7],
    		color:'#251c9f',
    		linewidth:1
    	},
 	{
-   		name : '郑国宝',
-   		value:[5,3,4,6,8,9],
+   		name : '情封',
+   		value:[5,6],
    		color:'#05ad74',
    		linewidth:1
    	}
