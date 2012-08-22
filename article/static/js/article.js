fbb0e3abc2255cb869afe59ca51d9df9c274ceda
f2er <uezheng@gmail.com>
2012-08-22 19:57:27 +0800

    update article

--- article/static/js/article.js
+++ article/static/js/article.js
@@ -6,30 +6,23 @@
 
 var Article = Article || {};
 Article.sidebar = {
-	CONFIG : {
-		sideID : 'js-side', /*左侧ID*/
-		targetClass : 'ar_item' /*目标对象类名*/
-	},
 	tpl : function(obj){
-		var that = this,
-			_ocontent = obj["content"],
-			_sideDiv = document.createElement('div');
-		_sideDiv.className = that.CONFIG.targetClass;
-		_sideDiv.setAttribute("data-id",obj.id);
+		var _ocontent = obj["content"];
+		var _sideDiv = document.createElement('div');
+			_sideDiv.className = "ar_item";
+			_sideDiv.setAttribute("data-id",obj.id);
 		var _html = '<h2>'+_ocontent.title+'</h2>';
 		_html +=	'<div class="info">';
 		_html +=		'<span class="name"><img src="'+_ocontent.avatar+'"/>'+_ocontent.author+'</span>';
 		_html +=		'<time>'+_ocontent.time+'</time>';
 		_html +=	'</div>';
 		_sideDiv.innerHTML = _html;
-		return _sideDiv;
+		document.getElementById('js-side').appendChild(_sideDiv);
 	},
 	sideRender : function(data){
-		var that = this,_flagment = document.createDocumentFragment();
 		for(var ai = 0;ai<data.length;ai++){
-			_flagment.appendChild(that.tpl( data[ai] ));
+			this.tpl( data[ai] );
 		}
-		document.getElementById( that.CONFIG.sideID ).appendChild(_flagment);
 	},
 	ajaxRender : function(data,n){
 		var _content = data[n]['content'];
@@ -61,20 +54,19 @@ Article.sidebar = {
 		document.getElementById('js-content').appendChild(loading);
 	},
 	clickAjax : function(data){
-		var that = this,
-			_jsContent = document.getElementById('js-content');
-		document.getElementById( that.CONFIG.sideID ).addEventListener('click',function(e){
+		var that = this;
+		document.getElementById('js-side').onclick = function(e){
 			e.stopPropagation();
 			var _target = e.target,_element = null;
 			
-			var _targetClass = _target.className;
-			if( _targetClass == that.CONFIG.targetClass ){
+			if(_target.className == "ar_item"){
 				_element = _target;
 			}else{
 				var _parent = that.getParent(_target);
-				if( _targetClass == that.CONFIG.targetClass ){
+				if(_parent.className == "ar_item"){
 					_element = _parent;
-				}else{
+				}
+				if(_parent.className !="ar_item"){
 					_element = that.getParent(_parent);
 				}
 			}
@@ -84,12 +76,11 @@ Article.sidebar = {
 			if( index == undefined){
 				return;
 			}
-			
 			var _url = data[index]['content']['ajaxcontent']['ajaxSource'];
 			var _html = that.ajaxRender(data,index);
-			var _tagName = document.getElementById( that.CONFIG.sideID ).querySelectorAll(that.CONFIG.targetClass);
+			var _tagName = document.getElementById('js-side').querySelectorAll('.ar_item');
 			for(var ci = data.length-1;ci>=0;ci--){
-				_tagName[ci].className = (ci==index) ? "ar_item current" : that.CONFIG.targetClass ;
+				_tagName[ci].className = (ci==index) ? "ar_item current" : "ar_item";
 			}
 			if(data[index]['content']['ajaxcontent']['ajaxFlag']){
 				that.createLoading();
@@ -97,14 +88,15 @@ Article.sidebar = {
 					_html += '<div class="ar_content">';
 					_html += txt;
 					_html +='</div>';
-					_jsContent.removeChild(document.getElementById('js-loading'));
-					_jsContent.innerHTML = _html;
+					document.getElementById('js-content').removeChild(document.getElementById('js-loading'));
+					document.getElementById('js-content').innerHTML = _html;
 				});
 			}else{
 				window.open("/article/page/"+idPro+".html");
+				//window.location.href = "/article/page/"+idPro+".html";
 			}
 			window.location.href="#"+idPro;
-		},false);
+		}
 	},
 	init : function(articleData){
 		this.sideRender(articleData);
