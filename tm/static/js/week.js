19f45ad7e01f559dae49f725f3fb1f25d1679efa
f2er <uezheng@gmail.com>
2012-08-22 21:56:54 +0800

    update tm

--- tm/static/js/week.js
+++ tm/static/js/week.js
@@ -7,25 +7,25 @@ var data_labels = ["星期一","星期二","星期三","星期四","星期五","
 var timeData = [
 	{
    		name : '小芳',
-   		value:[6,6，6.5],
+   		value:[6,6,6.5],
    		color:'#1f7e92',
    		linewidth:1
    	},
 	{
    		name : '俊GG',
-   		value:[5,7],
+   		value:[5,7,2],
    		color:'#f00',
    		linewidth:1
    	},
 	{
    		name : '小郭',
-   		value:[5,5],
+   		value:[5,5,6],
    		color:'#1c9f1c',
    		linewidth:1
    	},
 	{
    		name : '雪莲',
-   		value:[8,7],
+   		value:[8,7,6],
    		color:'#251c9f',
    		linewidth:1
    	},
