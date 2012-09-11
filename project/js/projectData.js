fe0b989b2357dc3dfee94d7737c4e16b7c6eeaf6
f2er <uezheng@gmail.com>
2012-09-11 22:01:42 +0800

    update project

--- project/js/projectData.js
+++ project/js/projectData.js
@@ -16,5 +16,18 @@ var projectData = [
 				"ajaxSource" : "introduction/001.html"
 		  }
 		}
+	},
+	{
+		"id":002,
+		"content" : {
+		  "url" : "http://www.4399ued.com/wiki/",
+		  "imgCover" : "http://img1.gtimg.com/ent/pics/hv1/205/153/1139/74102695.jpg",
+		  "createTime" : "2012-08-25",
+		  "title" : "4399UED WIKI",
+		  "state" : "进行中",
+		  "ajaxcontent" : {
+				"ajaxSource" : "introduction/001.html"
+		  }
+		}
 	}
 ]
