0700b112f0bf3d3b3c28ea976436995e9874d4cb
f2er <uezheng@gmail.com>
2012-11-25 20:23:33 +0800

    update uxweekly

--- debug/css/debug.js
+++
@@ -1,9 +0,0 @@
-javascript:(function(){
-	var _link = document.createElement('link');
-	_link.setAttribute('href','debug.css');
-	_link.setAttribute('rel','stylesheet');
-	_link.setAttribute('type','text/css');
-	if( typeof (_link) != undefined ){
-		document.getElementsByTagName('head')[0].appendChild(_link);
-	}
-})();
