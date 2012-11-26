64582780c9206d89b3f75234860e4c842a3578ec
f2er <uezheng@gmail.com>
2012-11-27 07:00:50 +0800

    update uxweekly

--- js/ux.js
+++ js/ux.js
@@ -20,7 +20,7 @@
         if( !isObject($(id)) ){
             return;
         }
-        var _html = '我来自头部';
+        var _html = '<a href="http://demo.4399ued.com/ued/ux/" title="返回首页">返回首页</a>';
         $(id).innerHTML = _html;
     }
 
@@ -29,18 +29,16 @@
             return;
         }
         var _html = '<div class="ux_footer">'+
-                    '<div class="xm_fe">'+
-                    '<img src="http://tp1.sinaimg.cn/2771882100/180/5631942496/0" width="71" height="71" alt="厦门前端联盟"/>厦门前端联盟</div>'+
-                    '<div class="ued">4399用户体验部门</div>'+
+           
+                    '<div><a  href="http://demo.4399ued.com/ued/ux/index.html"   class="ued"></a></div>'+
                     '<dl class="ux_about">'+
-                        '<dt>关于UX火花集</dt>'+
-                        '<dd>UX火花，是一份用以收集和分享UX资源、业界观点、创新与前瞻的邮件列表，欢迎有兴趣的同学共同参与组织与供稿。</dd>'+
-                        '<dd>地址：http://www.4399ued.com/ux/</dd>'+
+                        '<dt>关于4399UED周刊</dt>'+
+                        '<dd>4399UED周刊，是一份用以收集和分享UX资源、业界观点、创新与前瞻的分享列表，竭诚欢迎有兴趣的同学共同参与组织与供稿。</dd>'+
+                        '<dd>UED周刊小组成员:<a href="http://demo.4399ued.com/ued/ux/about.html" class="about">快～快点我</a></dd>'+
                     '</dl>'+
 
                     '</div>'+
-                    '<div class="ux_site">本站链接均来自于互联网，有兴趣参与的童鞋请联系 <span>@情封</span><span>@鬼懿</span>'+
-					'<scr'+'ipt type="text/javascript">var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cscript src=\'" + _bdhmProtocol + "hm.baidu.com/h.js%3Fd19e0ade259c89f350a75fb8e02cf042\' type="text/javascript"%3E%3C/script%3E"));</script>'
+                    '<div class="ux_site">本站链接均来自于互联网，有兴趣参与的童鞋请联系 <a target="_self" href="mailto:zhengguobao@4399.com">@情封</span>'+
                     '</div>';
         $(id).innerHTML = _html;
     }
---
+++ js/weekly/20121121.js
@@ -0,0 +1,62 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: 情封
+ * Date: 12-10-12
+ * Time: 上午10:47
+ * project list
+ */
+
+var ux_content = {
+    "topic":"用户体验路上有你我的精彩",
+    "topic_one" :{
+        "topic_title" : "优秀设计师应当知道的20大UI设计原则",
+        "topic_img_url" : "../../content/20121121/6.png",
+        "topic_url":"http://www.leiphone.com/0806-danice-ui-design-principles.html",
+        "topic_summary":"[核心提示]世界级图形设计大师Paul Rand（保罗.兰德）曾经说过：“设计绝不是简单的排列组合与简单地再编辑，它应当充满着价值和意义，去说明道理，去删繁就简，去阐明演绎，去修饰美化，去赞美褒扬，使其有戏剧意味，让人们信服你所言……”，由此可见，设计绝非轻而易举之事，优秀的设计更是难上加难..."
+    },
+    "leftSide":
+	{
+           "topic_title" : "iPhone 5/iOS 6前端开发指南",
+           "topic_url" : "http://www.qianduan.net/iphone5ios6-front-end-development-guide.html",
+           "topic_summary" : "iPhone 5和iOS 6已经发布好几天了，相信很多童鞋都已经刷上了iOS 6。我们在之前就发表过《iOS 6中Safari对HTML5的支持》，但是除此之外，实际上市的iOS 6具体支持还有哪些新功能？让我们一起看下。。iPhone首先屏幕变长了，分辨率从iPhone 4s的640px*960变成了640*1136，长宽比变成了16：9。DPI依然是326。其它表现看起来是一样的。"
+    },
+	
+	
+    "rightSide":[
+            {
+                "topic_title":"人人都懂交互设计！",
+                "topic_img_url":"../../content/20121121/5.png",
+                "topic_summary" : "人人都懂交互设计！这听起来确实是个好事儿：在达成共识，彼此理解的基础上，工作才能更好地开展。",
+                "topic_url" : "http://uxc.360.cn/archives/349.html"
+            },
+            {
+                "topic_title":"交互设计师到底是需要做什么?",
+                "topic_img_url":"../../content/20121121/4.png",
+                "topic_summary" : "交互设计师到底是需要做什么？尽管很多谈及交互的书上都已经回答过了：发现用户需要，建立明确需求；提出设计方案；制作设计原型；用户测试和评估。",
+                "topic_url" : "http://uedc.163.com/4816.html"
+            }
+        ],
+
+    "topic_three":[
+        {
+            "topic_title":"前端工程师的价值体现在哪里？",
+            "topic_img_url":"../../content/20121121/1.png",
+            "topic_url" : "http://blog.jobbole.com/30091/"
+        },
+        {
+            "topic_title":" 腾讯网无障碍说明",
+            "topic_img_url":"../../content/20121121/2.png",
+            "topic_url" : "http://www.qq.com/demo/accessibility.htm"
+        },
+        {
+            "topic_title":"目的，目标，解决方案",
+            "topic_img_url":"../../content/20121121/3.png",
+            "topic_url" : "http://ucdchina.com/snap/12271"
+        }
+    ],
+    "editor" : ["情封","滋繁"]
+
+}
+
+
+
