703f74f18aa7047b504451c87ea3795082ca93f6
f2er <uezheng@gmail.com>
2012-11-27 10:10:10 +0800

    update

--- js/ux.js
+++ js/ux.js
@@ -33,7 +33,7 @@
                     //'<div><a  href="http://demo.4399ued.com/ued/ux/index.html"   class="ued"></a></div>'+
                     '<dl class="ux_about">'+
                         '<dt>关于前端周刊</dt>'+
-                        '<dd>前端周刊，是一份用以收集和分享UX资源、业界观点、创新与前瞻的分享列表，竭诚欢迎有兴趣的同学共同参与组织与供稿。</dd>'+
+                        '<dd>前端周刊，是一份用以收集和分享UX资源、业界观点、创新与前瞻的小站，竭诚欢迎有兴趣的同学共同参与组织与供稿。</dd>'+
                         '<dd class="member">前端周刊小组成员:<a href="http://uxweekly.f2er.net/about.html" class="about">快～快点我</a></dd>'+
                     '</dl>'+
 
