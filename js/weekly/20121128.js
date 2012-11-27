89d2201dd0dc17155223690488ea586c3a4552f0
f2er <uezheng@gmail.com>
2012-11-27 19:20:58 +0800

    update tj

---
+++ js/weekly/20121128.js
@@ -0,0 +1,62 @@
+﻿/**
+ * Created with JetBrains PhpStorm.
+ * User: 情封
+ * Date: 12-10-12
+ * Time: 上午10:47
+ * project list
+ */
+
+var ux_content = {
+    "topic":"设计视觉，前端开发你怎么看？",
+    "topic_one" :{
+        "topic_title" : "",
+        "topic_img_url" : "../../content/20121128/6.png",
+        "topic_url":"http://ued.baidu.com/?p=3721",
+        "topic_summary":"[核心提示]服务设计不仅追求更为完善产品设计，也要求设计人员具备大局观、统筹力、创造力和跨界性。以提升产品本身的设计为起点，逐步扩大对设计全局的思考，让体验设计更加完整，从而产生更大的价值。"
+    },
+    "leftSide":
+	{
+           "topic_title" : "【前端】你未必知道的CSS故事：揭开leading的面纱",
+           "topic_url" : "http://www.ituring.com.cn/article/18076",
+           "topic_summary" : "几乎所有CSS书在讲到line-height属性的时候，都会提到传统排版中的一个术语——leading。这个leading到底指什么，它在汉字排版中叫什么，它与CSS中的line-height，也就是“行高”有什么关系？本文试图拔开历史的迷雾，把事实真相呈现在读者眼前。"
+    },
+	
+	
+    "rightSide":[
+            {
+                "topic_title":"【前端】当你输入一个网址的时候，实际会发生什么?",
+                "topic_img_url":"../../content/20121128/5.png",
+                "topic_summary" : "作为一个软件开发者，你一定会对网络应用如何工作有一个完整的层次化的认知，同样这里也包括这些应用所用到的技术：像浏览器，HTTP，HTML，网络服务器，需求处理等等。",
+                "topic_url" : "http://www.cnblogs.com/wenanry/archive/2010/02/25/1673368.html"
+            },
+            {
+                "topic_title":"【设计】图片格式与设计那点事儿",
+                "topic_img_url":"../../content/20121128/4.png",
+                "topic_summary" : "像油画家要了解他的颜料和画布、雕塑家要了解他的石材一样，作为网页设计师也应该对图片格式的特性有一定了解，这样才能更好的表达你的创意和想法。",
+                "topic_url" : "http://ued.taobao.com/blog/2010/12/10/jpg_png/"
+            }
+        ],
+
+    "topic_three":[
+        {
+            "topic_title":"【前端】移动终端开发必备知识",
+            "topic_img_url":"../../content/20121128/1.png",
+            "topic_url" : "http://isux.tencent.com/%E7%A7%BB%E5%8A%A8%E7%BB%88%E7%AB%AF%E5%BC%80%E5%8F%91%E5%BF%85%E5%A4%87%E7%9F%A5%E8%AF%86.html"
+        },
+        {
+            "topic_title":" 【项目管理】苏杰：最近对项目管理的一些实践",
+            "topic_img_url":"../../content/20121128/2.png",
+            "topic_url" : "http://www.woshipm.com/operate/11234.html"
+        },
+        {
+            "topic_title":"信息架构与用户体验",
+            "topic_img_url":"../../content/20121128/3.png",
+            "topic_url" : "http://isux.tencent.com/%E4%BF%A1%E6%81%AF%E6%9E%B6%E6%9E%84%E4%B8%8E%E7%94%A8%E6%88%B7%E4%BD%93%E9%AA%8C%E8%AE%BE%E8%AE%A1%E6%9C%89%E4%BD%95%E4%B8%8D%E4%B8%80%E6%A0%B7%EF%BC%9F.html"
+        }
+    ],
+    "editor" : ["情封","滋繁"]
+
+}
+
+
+
